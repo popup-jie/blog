27a464e777b67b29c4f37630dafbbb46c12536a7
shenjunjian <867427795@qq.com>
2019-07-09 15:35:19 +0800

    '新增常见面试题'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,45 +1,56 @@
 module.exports = {
-    title: '一个胖圆的程序员',
-    description: '一个胖圆的程序员',
-    port: 9999,
-    base: `/`,
-    themeConfig: {
-        editLinks: true,
-        displayAllHeaders: true,
-        nav: [{
-                text: '常见问题处理',
-                link: '/normal-problem/'
-            },
-            {
-                text: 'node相关学习',
-                link: '/node/'
-            }
-        ],
-        sidebar: {
-            '/node/': [{
-                title: 'node相关学习',
-                collapsable: false,
-                children: [
-                    ''
-                ]
-            }, ],
-            '/normal-problem/': [{
-                title: '常见问题处理',
-                collapsable: false,
-                children: [
-                    '',
-                    'vue-plugins',
-                    'browse-proxy',
-                    'js-normal',
-                    'console'
-                ]
-            }, ],
-            '/': [{
-                collapsable: false,
-                children: [
-                    ''
-                ]
-            }]
-        }
-    }
+	title: '一个胖圆的程序员',
+	description: '一个胖圆的程序员',
+	port: 9999,
+	base: `/`,
+	themeConfig: {
+		editLinks: true,
+		displayAllHeaders: true,
+		nav: [{
+			text: '常见问题处理',
+			link: '/normal-problem/'
+		},
+		{
+			text: 'node相关学习',
+			link: '/node/'
+		},
+		{
+			text: '前端100问',
+			link: '/front-question/'
+		}
+		],
+		sidebar: {
+			'/node/': [{
+				title: 'node相关学习',
+				collapsable: false,
+				children: [
+					''
+				]
+			},],
+			'/normal-problem/': [{
+				title: '常见问题处理',
+				collapsable: false,
+				children: [
+					'',
+					'vue-plugins',
+					'browse-proxy',
+					'js-normal',
+					'console'
+				]
+			}],
+			'/front-question/': [{
+				title: '前端100问',
+				collapsable: false,
+				children: [
+					''
+				]
+			}],
+			'/': [{
+				collapsable: false,
+				children: [
+					''
+				]
+			}]
+		}
+	}
 }
